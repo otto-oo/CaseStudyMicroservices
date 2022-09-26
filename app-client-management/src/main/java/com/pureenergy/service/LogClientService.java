4794ea5fe0eb4c90e47a9f9b17e55e14fc63e94b
otto <otto.o@cybertekschool.com>
2022-09-26 20:47:01 +0300

    Logs are done..

--- app-client-management/src/main/java/com/pureenergy/service/LogClientService.java
+++ app-client-management/src/main/java/com/pureenergy/service/LogClientService.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient("log-service")
+@FeignClient(name = "log-service")
 public interface LogClientService {
 
-    @PostMapping
+    @PostMapping("/api/v1/logs")
     LogResponseDTO createLog(@RequestBody LogDTO logDTO);
 }
